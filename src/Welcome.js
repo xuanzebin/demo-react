01faa74d48c5ab643249ddf9eeac8d68721e0360
xuanzebin <xuanzebin@126.com>
2018-07-28 23:49:54 +0800

    setState

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,8 +1,24 @@
 import React from 'react'; // 为什么要 import React
 class Welcome extends React.Component {
-  render() {
-    return <h1>Hello, {this.props.name}</h1>;
-  }
+    constructor(props){
+        super(props)
+        this.state={
+            date: new Date()
+        }
+        setInterval(()=>{
+            this.setState({
+                date:new Date()
+            })
+        })
+    }
+    render() {
+        return (
+            <div>
+              <h1>Hello, {this.props.name}</h1>
+              <h2>{this.state.date.toString()}</h2>
+            </div>
+        )
+    }
 }
 
 export default Welcome // 为什么要 export，为什么要加 default
