b104206fb2475dc557684089d267357486209798
xuanzebin <xuanzebin@126.com>
2018-07-29 10:24:02 +0800

    每个生命周期钩子都尝试setState

--- src/Welcome.js
+++ src/Welcome.js
@@ -8,20 +8,25 @@ class Welcome extends React.Component {
         }
         setInterval(()=>{
             this.setState(
-            //     {
-            //     date:new Date()
-            // })
-            (state,props)=>({date: new Date()}))
+                (state,props)=>({date: new Date()})
+            )
         })
         console.log('构造函数初始化')
     }
     componentWillMount(){
+        setInterval(()=>{
+            this.setState(
+                (state,props)=>({date: new Date()})
+            )
+        })
         console.log('马上就要render拉')
     }
-    componentDidMount(){
-        console.log('已经挂载到页面了')
-    }
     render() {
+        setInterval(()=>{
+            this.setState(
+                (state,props)=>({date: new Date()})
+            )
+        })
         console.log('这里是render')
         return (
             <div>
@@ -29,6 +34,54 @@ class Welcome extends React.Component {
               <h2>{this.state.date.toString()}</h2>
             </div>
         )
+    }
+    componentDidMount(){
+        setInterval(()=>{
+            this.setState(
+                (state,props)=>({date: new Date()})
+            )
+        })
+        console.log('已经挂载到页面了')
+    }
+    componentWillReceiveProps(){
+        setInterval(()=>{
+            this.setState(
+                (state,props)=>({date: new Date()})
+            )
+        })
+        console.log('我要读取props拉')
+    }
+    shouldComponentUpdate(){
+        setInterval(()=>{
+            this.setState(
+                (state,props)=>({date: new Date()})
+            )
+        })
+        console.log('请问要更新组件吗？')
+    }
+    componentWillUpdate(){
+        setInterval(()=>{
+            this.setState(
+                (state,props)=>({date: new Date()})
+            )
+        })
+        console.log('我要更新组件拉')
+    }
+    componentDidUpdate(){
+        setInterval(()=>{
+            this.setState(
+                (state,props)=>({date: new Date()})
+            )
+        })
+        console.log('组件更新完毕')
+    }
+    componentWillUnmount(){
+        setInterval(()=>{
+            this.setState(
+                (state,props)=>({date: new Date()})
+            )
+        })
+        console.log('我这个组件要死啦~')
     }
 }
 export default Welcome // 为什么要 export，为什么要加 default
